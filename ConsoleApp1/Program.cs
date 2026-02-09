5e0eda4afddaeb45614aa992b775e6c8723d9251
Raghad <Raghad@RAGHAD>
2026-02-09 18:16:51 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿double billAmount=Convert.ToDouble(Console.ReadLine());
+double finalPrice;
+
+
+if (billAmount >= 500)
+{
+    finalPrice = billAmount * 0.8;
+}
+else if (billAmount >= 300 && billAmount<=499)
+{
+    finalPrice = billAmount * 0.9;
+}
+else
+{
+    finalPrice = billAmount;
+}
+
+Console.Write($"Final Price: =( finalPrice)");
